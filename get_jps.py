a767f4a61b09d5813249e63229fb3cb0084cbd45
Isroel Kogan <isroelkogan@yahoo.com>
2017-12-25 19:32:13 -0500

    added config file and refactored get_jps accordingly

--- get_jps.py
+++ get_jps.py
@@ -1,14 +1,33 @@
+'''
+get_jps.py
+
+here put description of file
+'''
 import os
 from io import BytesIO
 from urllib.request import urlopen
 from zipfile import ZipFile
+import json
 
-current_directory = os.getcwd()
-final_directory = os.path.join(current_directory, r'JPS')
-if not os.path.exists(final_directory):
-	os.makedirs(final_directory)
+def get_jps(url, path):
+    '''
+    download jps bible located at url to path
+    '''
+    if os.path.exists(path):
+        print('"%s" exists. cancelling download.'%path)
+        return
+    os.mkdir(path)
+    with urlopen(url) as zipresp:
+        with ZipFile(BytesIO(zipresp.read())) as zfile:
+            zfile.extractall(path)
 
-zipurl = 'http://www.mechon-mamre.org/htmlzips/et002.zip'
-with urlopen(zipurl) as zipresp:
-    with ZipFile(BytesIO(zipresp.read())) as zfile:
-        zfile.extractall(final_directory)
+def main():
+    '''
+    runs get_jps() according to config file.
+    '''
+    with open("config.json") as config:
+        options = json.load(config)
+    get_jps(options["jps_url"], options["local_jps_html"])
+
+if __name__ == "__main__":
+    main()
